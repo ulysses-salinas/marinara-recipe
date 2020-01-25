1ffd6e55a812ba1fa913e33cb058f9ddd3a86ae9
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-01-25 13:44:20 -0600

    Second Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// hi - i am a comment
+
+function marinaraSauce () {
+console.log('Heat a medium-large saucepan over medium heat.')
+console.log('Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes')
+console.log('Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.')
+console.log('Cover the post, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes')
+console.log('Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.')
+
+}
+
+marinaraSauce()
