16115ed6ac267a2090e285a897a40fd0286b444e
Ulysses Salinas <ulysses.salinas88@gmail.com>
2020-01-25 14:02:52 -0600

    added Adam to script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // hi - i am a comment
 
-function marinaraSauce () {
+function marinaraSauce (cookName) {
+    
+    console.log('Hey,'  + cookName + '!' + 'This is my marinara sauce recipe!')
 console.log('Heat a medium-large saucepan over medium heat.')
 console.log('Add 2 tspns of oil and 5 carlic cloves, cook until golden, about 2 minutes')
 console.log('Add 1/4 cup water, 2 cans of crushed tomates, and salt and season with black pepper to taste.')
@@ -8,5 +10,5 @@ console.log('Cover the post, bring to a boil, reduce the heat to medium low, and
 console.log('Stir in 1/4 cup roughly chopped fresh basil, salt and pepper as needed.')
 
 }
-
-marinaraSauce()
+var cookName = 'Adam';
+marinaraSauce(cookName)
